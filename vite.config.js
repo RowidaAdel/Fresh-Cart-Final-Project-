8d3f198e8dcf1bf6769b3c94304a945ef9d4ce8d
RowidaAdel <rowidaadel036@gmail.com>
2025-07-21 02:34:53 +0300

    Fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Fresh-Cart-Final-Project-/',
+  base: '/',
   plugins: [tailwindcss(),
     react()],
 })
