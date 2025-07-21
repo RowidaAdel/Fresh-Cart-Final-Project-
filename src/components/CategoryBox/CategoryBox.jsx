8a04cff60d6e4e93085167f6b2b45971036abf11
RowidaAdel <rowidaadel036@gmail.com>
2025-07-21 03:21:47 +0300

    fix: تحسين الفوتر والسلايدر مع تحسين الأداء والـ preload

--- src/components/CategoryBox/CategoryBox.jsx
+++ src/components/CategoryBox/CategoryBox.jsx
@@ -4,7 +4,7 @@ export default function CategoryBox({ info }) {
   const { img, des, head, price } = info
   return (
     <div className="relative" >
-      <img src={img} className="w-full object-cover h-[500px] block" alt="SliderImage" rel="preload" as="image" href width={80} height={40}/>
+      <img src={img} alt="SliderImage" className="w-full object-cover h-[500px] block" width={1200} height={500} />
       <div className="layer sm:space-y-4 space-y-2  absolute top-0 left-0 md:w-2/3 w-4/6  h-full flex flex-col justify-center items-start p-3 sm:p-8">
         <span className="sm:text-lg text-md">Exclusive Offer <span className="bg-red-700 text-sm ml-2 p-1 font-semibold text-white rounded-xl">{des}%</span></span>
         <h3 className="sm:text-5xl text-3xl md:w-5/6 sm:py-2 font-semibold ">{head}</h3>
